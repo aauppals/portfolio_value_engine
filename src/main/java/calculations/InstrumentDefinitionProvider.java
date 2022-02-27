386a9266a582d20c828534ea2c4a0743ec29a478
Ahmad Uppal <ahmad.uppal@fdmgroup.com>
2022-02-27 14:27:33 +0800

    Refactoring SQL's for additional Stock values; take String as param for DB connections

--- src/main/java/calculations/InstrumentDefinitionProvider.java
+++ src/main/java/calculations/InstrumentDefinitionProvider.java
@@ -11,28 +11,27 @@ public class InstrumentDefinitionProvider {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
+        String dbPath = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
 
         try {
-            Connection connection = DriverManager.getConnection(url);
+            Connection connection = DriverManager.getConnection(dbPath);
             if (connection == null) {
                 throw new RuntimeException("Unable to make connection");
             }
-//            DatabaseMetaData meta = connection.getMetaData();
-//            System.out.println("The driver name is " + meta.getDriverName());
-//            System.out.println("A new database has been created");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void createNewTable() {
+    public static void createNewTable(String fileName) {
         //ToDo: Path below is hardcoded at the moment
-        String dbPath = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/Test.db";
+        String dbPath = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
 
-        String sqlStocks = "CREATE TABLE IF NOT EXISTS stocks(\n"
+        String sqlStocks = "CREATE TABLE IF NOT EXISTS Stocks(\n"
                 + " id integer PRIMARY KEY,\n"
                 + " ticker text NOT NULL,\n"
+                + " price real,\n"
+                + " expectedReturn real,\n"
                 + " volatility real\n"
                 + ");";
 
@@ -79,35 +78,37 @@ public class InstrumentDefinitionProvider {
 //        }
 //    }
 
-    public static void insertStocks(String ticker, double volatility) {
-        String sql = "INSERT INTO Stocks(ticker, volatility) VALUES(?,?)";
+    public static void insertStocks(String fileName, String ticker, double price, double expectedReturn, double volatility) {
+        String sql = "INSERT INTO Stocks(ticker, price, expectedReturn, volatility) VALUES(?,?,?,?)";
 
         try {
             //ensureConnectionOpened();
-            String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/Test.db";
+            String dbPath = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
             try {
-                conn = DriverManager.getConnection(url);
+                conn = DriverManager.getConnection(dbPath);
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
 
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
             preparedStatement.setString(1, ticker);
-            preparedStatement.setDouble(2, volatility);
+            preparedStatement.setDouble(2, price);
+            preparedStatement.setDouble(3, expectedReturn);
+            preparedStatement.setDouble(4, volatility);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void insertOptions(String ticker, String maturityDate, double strikePrice,
+    public static void insertOptions(String fileName, String ticker, String maturityDate, double strikePrice,
                                      String optionType, double interestRate, double volatility) {
 
         String sql = "INSERT INTO Options(ticker, maturityDate, strikePrice, optionType, interestRate, volatility) VALUES(?,?,?,?,?,?)";
 
         try {
 //            ensureConnectionOpened();
-            String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/Test.db";
+            String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
             try {
                 conn = DriverManager.getConnection(url);
             } catch (SQLException e) {
@@ -121,14 +122,15 @@ public class InstrumentDefinitionProvider {
             preparedStatement.setDouble(5, interestRate);
             preparedStatement.setDouble(6, volatility);
             preparedStatement.executeUpdate();
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void getAllStocks() {
+    public static void getAllStocks(String fileName) {
 
-        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/Test.db";
+        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
         try {
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
@@ -144,6 +146,8 @@ public class InstrumentDefinitionProvider {
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + "\t" +
                         resultSet.getString("ticker") + "\t" +
+                        resultSet.getDouble("price") + "\t" +
+                        resultSet.getDouble("expectedReturn") + "\t" +
                         resultSet.getDouble("volatility"));
             }
         } catch (SQLException e) {
@@ -152,8 +156,8 @@ public class InstrumentDefinitionProvider {
     }
 
 
-    public static void deleteAllStocks() {
-        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/Test.db";
+    public static void deleteAllStocks(String fileName) {
+        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
         try {
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
@@ -169,9 +173,9 @@ public class InstrumentDefinitionProvider {
     }
 
 
-    public static void getAllOptions() {
+    public static void getAllOptions(String fileName) {
 
-        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/Test.db";
+        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
         try {
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
@@ -200,8 +204,8 @@ public class InstrumentDefinitionProvider {
         }
     }
 
-    public static void deleteAllOptions() {
-        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/Test.db";
+    public static void deleteAllOptions(String fileName) {
+        String url = "jdbc:sqlite:C:/Users/N B C/Desktop/portfolio_value_engine/" + fileName;
         try {
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
--- src/test/java/calculations/InstrumentDefinitionProviderTest.java
+++ src/test/java/calculations/InstrumentDefinitionProviderTest.java
@@ -19,17 +19,18 @@ public class InstrumentDefinitionProviderTest {
 
     @Test
     public void WHEN_table_is_created_and_records_inserted_THEN_correct_entries_are_made() {
-        InstrumentDefinitionProvider.deleteAllStocks();
-        InstrumentDefinitionProvider.deleteAllOptions();
-        InstrumentDefinitionProvider.insertStocks("AAPL", 0.01);
-        InstrumentDefinitionProvider.insertStocks("TELSA", 0.01);
-        InstrumentDefinitionProvider.insertOptions("AAPL", "OCT-2020", 110, "C", 0.02, 0.01);
-        InstrumentDefinitionProvider.insertOptions("AAPL", "OCT-2020", 110, "P", 0.02, 0.01);
-        InstrumentDefinitionProvider.insertOptions("TELSA", "OCT-2020", 400, "C", 0.02, 0.01);
-        InstrumentDefinitionProvider.insertOptions("TELSA", "OCT-2020", 400, "P", 0.02, 0.01);
+        InstrumentDefinitionProvider.deleteAllStocks("Test.db");
+        InstrumentDefinitionProvider.deleteAllOptions("Test.db");
+        InstrumentDefinitionProvider.createNewTable("Test.db");
+        InstrumentDefinitionProvider.insertStocks("Test.db","AAPL", 50,0.5,0.5);
+        InstrumentDefinitionProvider.insertStocks("Test.db","TELSA", 75,0.75,0.75);
+        InstrumentDefinitionProvider.insertOptions("Test.db","AAPL", "OCT-2020", 110, "C", 0.02, 0.01);
+        InstrumentDefinitionProvider.insertOptions("Test.db","AAPL", "OCT-2020", 110, "P", 0.02, 0.01);
+        InstrumentDefinitionProvider.insertOptions("Test.db","TELSA", "OCT-2020", 400, "C", 0.02, 0.01);
+        InstrumentDefinitionProvider.insertOptions("Test.db","TELSA", "OCT-2020", 400, "P", 0.02, 0.01);
 
-        InstrumentDefinitionProvider.getAllStocks();
-        InstrumentDefinitionProvider.getAllOptions();
+        InstrumentDefinitionProvider.getAllStocks("Test.db");
+        InstrumentDefinitionProvider.getAllOptions("Test.db");
 
     }
 
